2c3b20271a87ddf5857e29e921731440b35eef4c
James Kazuza Jr <webnauta01@gmail.com>
2021-05-14 02:14:01 -0300

    Oddsmaker file

---
+++ oddsmaker.py
@@ -0,0 +1,90 @@
+import pyfiglet
+from termcolor import colored
+import os
+import argparse
+
+parser = argparse.ArgumentParser(description = "This program helps to estimate expected value of football betting.")
+parser.add_argument("-v", action = "version", version = "Oddsmaker 1.0")
+msg = parser.parse_args()
+
+os.system("clear")
+
+global bookie_odds
+global bookie_probs 
+global my_odds
+global my_probs 
+global diff_odds
+global profit
+      
+def results():
+
+    print("*" * 50)
+    title = "ODDs Comparision"
+    print(title.center(45))
+    print("")
+    print("*" * 50)
+
+    print("=" * 50) 
+    txt1 = "Bookie Odd: {:.2f} {:.2f}%".format(bookie_odds,bookie_probs)
+    print(txt1.center(50)) 
+    print("-" * 50)
+    txt2 = "My Odd: {:.2f} {:.2f}%".format(my_odds,my_probs) 
+    print(txt2.center(50))
+    print("-" * 50)
+
+    if (diff_odds > 0):
+        txt3 = "Positive Expected Value: +{:.2f}% (profit)".format(profit) 
+        print(colored(txt3.center(50), "green"))
+    elif (diff_odds == 0):
+        txt4 = "Null Expected Value: {:.2f}% (breakeven)".format(profit) 
+        print(colored(txt4.center(50), "yellow"))
+    else:
+        txt5 = "Negative Expected Value: {:.2f}% (loss)".format(profit) 
+        print(colored(txt5.center(50), "red"))  
+    
+    print("-" * 50)
+
+while True:
+
+    try:
+        print("=" * 74)        
+        intro = pyfiglet.figlet_format("Football Betting")
+        print(intro)
+        print("=" * 60)
+        print("Use decimal point (.) instead of decimal comma (,)".center(60))
+        print("=" * 60)
+        bookie_odds = float(input("Bookmaker Odd (decimal): "))
+        bookie_probs = float(100/bookie_odds) #Convert to implied probabilities (0.00%)
+        my_probs = float(input("Your Estimated Probabilities(%): "))
+        my_odds = float(100/my_probs) #Convert to decimal odds (1.00)
+        diff_odds = (bookie_odds - my_odds)
+        profit = (diff_odds * 100)
+        os.system("clear")
+        results()        
+
+        question = str(input("Want to continue? [y,n]: ")).lower()            
+        os.system("clear")                        
+
+        if (question == "n"):
+            print("-" * 50)
+            print("Finished... Good luck!".center(50))
+            print("-" * 50)
+            exit()                
+        elif (question != "y"):
+            print("=" * 74)    
+            print("Type 'y' for YES or 'n' for NO (in the last question)...".center(74))
+            print("=" * 74)    
+            question = str(input("Want to continue? [y,n]: ")).lower()
+            if (question == "n"):
+                print("-" * 50)
+                print("Finished... Good luck!".center(50))
+                print("-" * 50)
+                exit()
+            elif (question != "y"):
+                print("-" * 50)
+                print("Try again".center(50))
+                print("-" * 50)
+                exit()         
+
+    except ValueError:
+        os.system("clear")
